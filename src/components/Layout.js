33b0cdcd79ff0476d1710d8c564bb55e5d410d0b
SoftDev-Abay <abay.aliev.03@gmail.com>
2024-01-11 20:57:30 +0600

    added authorization check

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,30 +1,49 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Topbar from "./Topbar";
 import LeftSidebar from "./LeftSidebar";
 import RightSidebar from "./RightSidebar";
 import BottomBar from "./BottomBar";
+import { useNavigate } from "react-router-dom";
+import { useAuthContext } from "../context/AuthContext";
 
 const Layout = ({ children, chat = false }) => {
-  return (
-    <main className="main">
-      <LeftSidebar />
-      <section className="main-container">
-        {!chat && <Topbar />}
-        <div
-          className="main-container-content"
-          style={
-            chat
-              ? { marginTop: "0px", padding: "0px", marginBottom: "0px" }
-              : {}
-          }
-        >
-          {children}
-        </div>
-        <BottomBar />
-      </section>
-      <RightSidebar />
-    </main>
-  );
+  const navigate = useNavigate();
+  const currentPath = window.location.pathname;
+
+  const { user } = useAuthContext();
+
+  useEffect(() => {
+    if (!user && currentPath !== "/login") {
+      navigate("/login");
+    }
+  }, [user]);
+
+  if (!user && currentPath !== "/login") {
+    <>
+      <p>loading...</p>
+    </>;
+  } else {
+    return (
+      <main className="main">
+        <LeftSidebar />
+        <section className="main-container">
+          {!chat && <Topbar />}
+          <div
+            className="main-container-content"
+            style={
+              chat
+                ? { marginTop: "0px", padding: "0px", marginBottom: "0px" }
+                : {}
+            }
+          >
+            {children}
+          </div>
+          <BottomBar />
+        </section>
+        <RightSidebar />
+      </main>
+    );
+  }
 };
 
 export default Layout;
